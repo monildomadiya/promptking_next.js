630e96472550408f8c1844fb44fa32d2b79a1a24
Monil Domadiya <example@example.com>
2026-06-09 17:03:31 +0530

    Remove vite-plugin-compression to fix deployment white screen

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    viteCompression({
-      algorithm: 'brotliCompress',
-      ext: '.br',
-    }),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz',
-    })
+    react()
   ],
   server: {
     host: true,
@@ -29,9 +20,6 @@ export default defineConfig({
     }
   },
   build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    cssMinify: true,
     chunkSizeWarningLimit: 1000
   }
 })
